b6d2208692e7eefa553a1675b24ba2ff8e54605f
Rilton Leão <riltonjunior@gmail.com>
2020-05-31 22:14:18 -0300

    Notification added.

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -85,11 +85,15 @@
             color="primary" 
             icon="pause_circle_filled" 
             dense
+          >
+          <q-icon
+            name="fiber_manual_record" class="record-badge absolute-top-right text-red"
           />
+          </q-btn>
         </q-item-section>
 
         <q-item-section
-        v-if="task.done"
+        v-if="task.done && !task.running"
         side
         >
           <q-btn 
@@ -115,9 +119,9 @@
 
 <script>
 import { date } from 'quasar'
-import * as Moment from 'moment'
-import 'moment/locale/pt-br'
+import * as moment from 'moment'
 import 'moment-duration-format'
+import 'moment/locale/pt-br'
 import Timer from '../components/Timer.vue'
 
 export default {
@@ -129,6 +133,7 @@ export default {
     return {
       /* Task */
       newTask: '',
+      timing: false,
       tasks: [
         {
           title: 'Fazer as compras',
@@ -144,9 +149,9 @@ export default {
           title: 'Atualizar currículos',
           done: true,
           createdAt: 1590953072486,
-          timeStarted: '',
-          timeStopped: '',
-          timeDuration: 0,
+          timeStarted: 1590966264096,
+          timeStopped: 1590966276274,
+          timeDuration: 12178,
           running: false
         },
         {
@@ -176,42 +181,58 @@ export default {
         persistent: true
       }).onOk(() => {
         this.tasks.splice(index, 1)
-        this.$q.notify('Tarefa apagada');
+        this.$q.notify({ type: 'positive', message: `Tarefa apagada com sucesso.` })
       })
     },
     addTask() {
-      this.tasks.push({
+      if (this.newTask === '') {
+      this.$q.notify({ type: 'warning', message: `Dê um nome a tarefa para adicioná-la.` })
+      } else {
+        this.tasks.push({
         title: this.newTask,
         done: false,
-        createdAt: Date.now()
+        createdAt: Date.now(),
+        timeStarted: '',
+        timeStopped: '',
+        timeDuration: 0,
+        running: false
       })
-      this.$q.notify('Tarefa adicionada');
+      this.$q.notify({ type: 'positive', message: `Tarefa adicionada com sucesso.` })
       this.newTask = ''
+      }
+      
     },
     startTracking(index) {
-      if (!this.tasks[index].timeStarted) {
-        this.tasks[index].timeStarted = Date.now()
+      if (this.timing === true) {
+        this.$q.notify({ type: 'error', message: `Você deve parar a tarefa em andamento para iniciar outra tarefa.` })
+
+      } else {
+        const tasksRef = this.tasks[index]
+        if (!tasksRef.timeStarted) {
+            tasksRef.timeStarted = Date.now()
+        }
+        tasksRef.running = true
+        this.timing = true
+        this.$q.notify({ type: 'info', message: `Registro de tempo iniciado.` })
       }
-      this.tasks[index].running = true
-      console.log(this.tasks[index])
     },
     stopTracking(index) {
-      if (!this.tasks[index].timeStopped) {
-        this.tasks[index].timeStopped = Date.now()
+      const tasksRef = this.tasks[index]
+      if (!tasksRef.timeStopped) {
+        tasksRef.timeStopped = Date.now()
       }
-      this.tasks[index].timeDuration += (this.tasks[index].timeStopped - this.tasks[index].timeStarted)
-      this.tasks[index].running = false
-      const timeDuration = this.tasks[index].timeDuration
-      // console.log(Moment.duration(timeDuration, 'milliseconds').format("d[d] h[h] m[m] s[s]", {trim: "both"}))
-      console.log(timeDuration)
+      tasksRef.timeDuration += (tasksRef.timeStopped - tasksRef.timeStarted)
+      tasksRef.running = false
+      this.timing = false
+      this.$q.notify({ type: 'info', message: `Registro de tempo parado.` })
     }
   },
   filters: {
     secondsAgo(value) {
-     return Moment(value).fromNow()
+     return moment(value).fromNow()
     },
     durationFormat(value) {
-     return Moment.duration(value, 'milliseconds').format("d[d] h[h] m[m] s[s]", {trim: "both"})
+     return moment.duration(value, 'milliseconds').format("d[d] h[h] m[m] s[s]", {trim: "both"})
     }
   }
 }
@@ -226,5 +247,26 @@ export default {
 }
 .no-tasks {
   opacity: .5;
+}
+@keyframes record-badge{
+  0%{
+    opacity: 0;
+  }
+  50%{
+    opacity: 1;
+  }
+  100%{
+    opacity: 0;
+  }
+}
+
+.record-badge{
+    animation-name: record-badge;
+    animation-duration: 2000ms;
+    animation-fill-mode: both;
+    animation-iteration-count: infinite;
+    font-size: 0.8rem !important;
+    top: 2px;
+    right: 2px
 }
 </style>
