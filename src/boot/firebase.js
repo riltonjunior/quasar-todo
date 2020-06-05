a2ff5e0cc20d68bcb624f4b876441513542c40f1
Rilton Leão <riltonjunior@gmail.com>
2020-06-05 15:54:41 -0300

    auth ident

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -4,13 +4,7 @@ import 'firebase/auth'
 // import 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCJVTGkyiwkuST0oBUorptAc4driQGCjGM',
-  authDomain: 'planme-app.firebaseapp.com',
-  databaseURL: 'https://planme-app.firebaseio.com',
-  projectId: 'planme-app',
-  storageBucket: 'planme-app.appspot.com',
-  messagingSenderId: '456090756148',
-  appId: '1:456090756148:web:916272f90a80a996e29661'
+
 }
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig)
